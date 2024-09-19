dc229ae3a070092fc634992cd5e6419a55e5ff9a
Juan Manuel Losada <juanmalosada01@gmail.com>
2024-09-18 22:16:41 -0300

    Cambios nuevos

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -1,14 +1,13 @@
 package ui;
 
-import instancia.ClarinInformationSource;
 import noticracia.core.Noticracia;
 
 import javax.swing.*;
 
 public class Main {
 
-    public static void main(String[] args) { //TODO: Que mandamos acá?
-        Noticracia noticracia = new Noticracia(new ClarinInformationSource());
+    public static void main(String[] args) {
+        Noticracia noticracia = new Noticracia("src/main/java/resources/extensions");
 
         SwingUtilities.invokeLater(() -> {
             NoticraciaView noticraciaView = new NoticraciaView(noticracia);
